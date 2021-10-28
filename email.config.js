c1965b62664d33e3eb771748f6cb9711120c1c93
qqqq0000 <1375580763@qq.com>
2021-10-28 15:04:44 +0800

    1.1.0

---
+++ email.config.js
@@ -0,0 +1,6 @@
+// 腾讯配置
+module.exports = {
+  smtp: "smtp.qq.com",
+  mailFrom: "1375580763@qq.com",
+  mailPwd: ""
+}
---
+++ message.config.js
@@ -0,0 +1,8 @@
+// 这里采用的是腾讯配置
+module.exports = {
+    "SmsSdkAppId": "",
+    "TemplateId": "",
+    "SecretId": "",
+    "SecretKey": "",
+    "SignName": ""
+}
