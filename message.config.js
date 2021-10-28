5605252833e1eafc4a64e68548b7176c8a200a4d
qqqq0000 <1375580763@qq.com>
2021-10-28 15:08:57 +0800

    1.1.0

--- message.config.js
+++ message.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    "SmsSdkAppId": "1400587018",
-    "TemplateId": "1172340",
-    "SecretId": "AKID86oGSP6WfZCYdRyEDwOZU8WdT2LOBhwv",
-    "SecretKey": "jCOL16wWpseAPM3SbU6RXMuBCwPbWCbj",
-    "SignName": "美丽宛如大海"
+    "SmsSdkAppId": "",
+    "TemplateId": "",
+    "SecretId": "",
+    "SecretKey": "",
+    "SignName": ""
 }
