ffa45d756eedc0e4fecdd80fc1db0fe9b2817356
liaoweizhong <1375580763@qq.com>
2021-11-09 10:03:41 +0800

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,14 +1,8 @@
 module.exports = {
-
-    
     user     : 'root',
-    // password : '@Liao5249',
     password: 'x5',
-    // host     : '101.32.181.63',
     host: '127.0.0.1',
     port: '3306',
-    // database: 'chaoliumeihai',
     database: 'nodeSpring',
     type: "mysql"
-
 } 
